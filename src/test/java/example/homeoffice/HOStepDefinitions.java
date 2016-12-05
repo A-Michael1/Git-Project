faca21cbb920ae73e9cf1841bbeab1037dd9b24a
Abraham Michael <Abraham Michael>
2016-12-05 15:17:27 +0000

    Minor updates

--- src/test/java/example/homeoffice/HOStepDefinitions.java
+++ src/test/java/example/homeoffice/HOStepDefinitions.java
@@ -3,7 +3,6 @@ package example.homeoffice;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import example.wikipedia.WikipediaComputingPage;
 import uk.gov.homeoffice.virtuoso.framework.test.SeleniumTest;
 
 
